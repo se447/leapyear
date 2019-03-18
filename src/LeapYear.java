97c6729bb4f9e575b8607de72810f271d489d34b
Saad E <21321555+se447@users.noreply.github.com>
2019-03-18 14:04:26 -0400

    Calculated leap year

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -6,6 +6,19 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        //leap year is divisible by 4
+        //for years divisible by 100, they are only leap years if divisible by 400
+        //otherwise it isn't a leap year
         
+        System.out.println("Type a year: ");
+        int year = Integer.parseInt(reader.nextLine()); //read user input
+        
+        if (year % 100 == 0 && year % 400 == 0) {
+            System.out.println("The year is a leap year.");
+        } else if (year % 100 != 0 && year % 4 == 0) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
     }
 }
